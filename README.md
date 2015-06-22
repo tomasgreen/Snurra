e077dbc45a8fcc54b35660b1a76dee5d29c8d8bd
tomasgreen <tomas.green@gmail.com>
2015-06-22 21:49:40 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # Snurra
 Snurra - A JS button spinner
+
+
+
+## Snurra and destroy!
+
+Requires no setup of fancy classes or containers and leaves nothing behind once destroyd.
+
+```js
+var btn = document.getElementById('myButton');
+var s = Snurra(btn);
+/* do something useful */
+get('my api call',function(data){
+    /* finish up and destroy*/
+    s.destroy();
+    /* all done */
+});
+```
+
+## Want to do it the hard way?
+
+Pass an element or string as first argument and an object with your options in the other.
+
+```js
+var btn = Snurra('#myButton',{
+    autoStart: false
+});
+btn.start();
+get('my api call',function(data){
+    /* finish up and save your instance for later use */
+    btn.stop();
+});
+````
+
+## How about...
+The built in tap-event works well with desktop and touch devices.
+
+```js
+Snurra.globals.autoStart = false;
+var btn = Snurra('#myButton');
+btn.onTap(function(ev){
+    get('my api call',function(data){
+        btn.stop();
+    });
+});
+```
+
+## Custom SVGs
+Add your own image to the stack and use it as a default.
+
+```js					
+Snurra.addSpinner('/static/bubbles.svg');
+Snurra.gobals.maxSize = 40;
+```
+
+## Style with CSS
+Change colors and more with CSS.
+
+```css
+.btn-red .snurra-img {
+    fill: yellow
+}
+```
---
+++ src/example/js/core.js
@@ -0,0 +1,127 @@
+var entityMap = {
+	"&": "&amp;",
+	"<": "&lt;",
+	">": "&gt;",
+	'"': '&quot;',
+	"'": '&#39;',
+	"/": '&#x2F;'
+};
+
+function escapeHtml(string) {
+	return String(string).replace(/[&<>"'\/]/g, function (s) {
+		return entityMap[s];
+	});
+}
+
+function addEvent(el, events, func, useCapture) {
+	if (useCapture === undefined) useCapture = false;
+	var arr = events.split(' ');
+	for (var i in arr) {
+		el.addEventListener(arr[i], func, useCapture);
+	}
+}
+
+function _on(el, events, func, useCapture) {
+	if (!el || (el.length === 0 && el != window)) return;
+	if (useCapture === undefined) useCapture = false;
+	if (el.length) {
+		for (var i = 0; i < el.length; i++) {
+			_on(el[i], events, func, useCapture);
+		}
+		return;
+	}
+	var ev = events.split(' ');
+	for (var e in ev) {
+		el.addEventListener(ev[e], func, useCapture);
+	}
+}
+
+function _stopEventPropagation(e) {
+	if (typeof e.stopPropagation === 'function') {
+		e.stopPropagation();
+		e.preventDefault();
+	} else if (window.event && window.event.hasOwnProperty('cancelBubble')) {
+		window.event.cancelBubble = true;
+	}
+}
+
+function _tapOn(el, func) {
+	if (el.ontouchstart === undefined) {
+		_on(el, 'click', func);
+		return;
+	}
+	var t = false;
+	_on(el, 'touchstart', function (ev) {
+		t = true;
+	});
+	_on(el, 'touchend', function (ev) {
+		if (t) {
+			func(ev);
+			_stopEventPropagation(ev);
+		}
+	});
+	_on(el, 'touchcancel touchleave touchmove', function (ev) {
+		t = false;
+	});
+}
+
+function example1() {
+	var btn = document.getElementById('example1');
+	_tapOn(btn,function(){
+		var s = new Snurra(btn);
+		setTimeout(function(){
+			s.destroy();
+		},3000);
+	});
+}
+function example2() {
+	var s = new Snurra('#example2',{
+		autoStart: false
+	});
+	_tapOn(s.el,function(){
+		s.start();
+		setTimeout(function(){
+			s.stop();
+		},3000);
+	});
+}
+function example3() {
+	var s = new Snurra('#example3',{
+		autoStart: false
+	});
+	s.onTap(function(){
+		setTimeout(function(){
+			s.stop();
+		},3000);
+	});
+}
+function example4() {
+	var btn = document.getElementById('example4');
+	_tapOn(btn,function(){
+		var s = new Snurra('#example4',{
+			spinner: '/static/bubbles.svg',
+			maxSize: 40
+		});
+		setTimeout(function(){
+			s.destroy();
+		},3000);
+	});
+}
+function example5 () {
+	var btn = document.getElementById('example5');
+	_tapOn(btn,function(){
+		var s = new Snurra(btn);
+		setTimeout(function(){
+			s.destroy();
+		},3000);
+	});
+}
+document.addEventListener('DOMContentLoaded', function () {
+	hljs.initHighlightingOnLoad();
+	Snurra.addSpinner('/static/bubbles.svg');
+	example1();
+	example2();
+	example3();
+	example4();
+	example5();	
+});
